086fe4ab4339b9ce6fda353a650453713d5c1035
박정연 <parkjyeon2808@gmail.com>
2022-10-10 22:02:15 +0900

    add a, b function

--- Project1/src/Main1.java
+++ Project1/src/Main1.java
@@ -1,5 +1,26 @@
 public class Main1 {
     public static void main(String[] args) {
         System.out.println("this is project1");
+
+        a(1, 4);
+
+        System.out.println(b(1));
+        System.out.println(b(4));
+
+    }
+
+
+    public static void a(int i, int j){
+        System.out.println(i+j);
+        System.out.println("====");
+        return;
+    }
+
+    public static boolean b(int c){
+        if(c == 1){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
