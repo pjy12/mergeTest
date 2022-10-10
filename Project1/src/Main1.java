1a268dde6e4327d02d05ec49ed63c669907dc6bd
박정연 <parkjyeon2808@gmail.com>
2022-10-10 22:17:16 +0900

    add function f

--- Project1/src/Main1.java
+++ Project1/src/Main1.java
@@ -2,25 +2,30 @@ public class Main1 {
     public static void main(String[] args) {
         System.out.println("this is project1");
 
-        a(1, 4);
+        a(1, 4, 5);
 
-        System.out.println(b(1));
-        System.out.println(b(4));
+        System.out.println(e(1));
+        System.out.println(e(4));
 
     }
 
 
-    public static void a(int i, int j){
+    public static void a(int i, int j, int k){
         System.out.println(i+j);
         System.out.println("====");
+        System.out.println(i+k);
         return;
     }
 
-    public static boolean b(int c){
+    public static boolean e(int c){
         if(c == 1){
             return true;
         }else{
             return false;
         }
+    }
+
+    public static double f(int k, int m){
+        return k/m;
     }
 }
