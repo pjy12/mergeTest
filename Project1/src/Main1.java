3966f7bc86ddcf5d486a5f01b8c568ca2f5abc05
박정연 <parkjyeon2808@gmail.com>
2022-10-10 22:26:26 +0900

    Revert "resolve"
    This reverts commit a85eb31f1475c238ea34c2ae985049cd44fec1f0.

--- Project1/src/Main1.java
+++ Project1/src/Main1.java
@@ -2,22 +2,21 @@ public class Main1 {
     public static void main(String[] args) {
         System.out.println("this is project1");
 
-        a(1, 4, 5);
+        a(1, 4);
 
-        System.out.println(e(1));
-        System.out.println(e(4));
+        System.out.println(b(1));
+        System.out.println(b(4));
 
     }
 
 
-    public static void a(int i, int j, int k){
+    public static void a(int i, int j){
         System.out.println(i+j);
         System.out.println("====");
-        System.out.println(i+k);
         return;
     }
 
-    public static boolean e(int c){
+    public static boolean b(int c){
         if(c == 1){
             return true;
         }else{
@@ -31,9 +30,5 @@ public class Main1 {
 
     public static int d(int a, int c){
         return a-c;
-    }
-
-    public static double f(int k, int m){
-        return k/m;
     }
 }
